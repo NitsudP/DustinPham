a0b1efd10e50fae2a2f55c66ea4d615b69b191b9
NitsudP <75590250+NitsudP@users.noreply.github.com>
2020-12-16 19:34:14 -0600

    Delete create-tab.js

--- create-tab.js
+++
@@ -1,8 +0,0 @@
-/**
- * @Portfolio createTab
- * type Function
- * @description Create one Tab automatically
- * @param {String} tabID The id of the tab links elements
- * @param {String} tabContentId The id of the content tab view
- * return void
- */
